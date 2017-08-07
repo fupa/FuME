b612348db7e1e0c0103feb9d7480d388d7327704
Andreas Feldl <fume@afeldl.de>
2017-08-07 22:19:40 +0200

    update

--- bin/uic5all.sh
+++ bin/uic5all.sh
@@ -3,4 +3,5 @@
 pyuic5 mainwindow.ui -o ../fume/ui/mainwindow.py;
 pyuic5 settings.ui -o ../fume/ui/settings.py;
 pyuic5 log.ui -o ../fume/ui/log.py;
-pyuic5 filter.ui -o ../fume/ui/filter.py
+pyuic5 filter.ui -o ../fume/ui/filter.py;
+pyuic5 about.ui -o ../fume/ui/about.py;
